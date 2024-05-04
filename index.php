789354d616541f70c5bf8375c692269e809eb58a
Ulises Desentis <ulifly@hotmail.com>
2024-05-04 11:08:06 -0600

    added HTML skeleton

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
-<h1>
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Marvel</title>
+</head>
+<body>
+    <div class ="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="container-fluid py-4">
+            <h1 class="header-main-title">Marvel Comics Characters</h1>
+            <form id="connectionForm">
+                <div class="form-group">
+                    <input required type="text" name="name" id="name" class="form-control-lg" placeholder="ex. Ironman, spiderman, cyclops">
+                </div>
+                <input type="submit" value="search" class="btn btn btn-secondary mb-2">
+            </form>
+        </div>
 
-?>
-</h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
